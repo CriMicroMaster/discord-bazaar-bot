26543fc465ec41ec4f0cbf309c7f00b44ce854cc
CriMicroMaster <lukasimhof06@hotmail.com>
2024-08-29 12:58:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { Client, GatewayIntentBits, ActivityType } = require("discord.js");
 const { Sequelize, DataTypes } = require("sequelize");
 const slash_deploy = require("./slash_deploy.js")
 
+require('dotenv').config();
+
 // Initialize the Discord client
 const client = new Client({
   intents: [
