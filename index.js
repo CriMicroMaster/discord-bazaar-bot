e4720efc074623c7711d81b30cea6e47f149c93d
CriMicroMaster <lukasimhof06@hotmail.com>
2024-10-31 16:12:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -234,14 +234,19 @@ client.on("interactionCreate", async (interaction) => {
           warningList += "No warnings found.";
         } else {
           for (const wallet of wallets) {
-            // Check if warnings is null and set it to 0 if it is
-            if (wallet.warnings === null) {
-              wallet.warnings = 0; // Set warnings to default value
-              await wallet.save(); // Save the changes to the database
-            }
+            // Check if userId is valid
+            if (wallet.userId) {
+              // Check if warnings is null and set it to 0 if it is
+              if (wallet.warnings === null) {
+                wallet.warnings = 0; // Set warnings to default value
+                await wallet.save(); // Save the changes to the database
+              }
     
-            // Append user warnings to the warning list
-            warningList += `User <@${wallet.userId}> has ${wallet.warnings} warnings.\n`;
+              // Append user warnings to the warning list
+              warningList += `User <@${wallet.userId}> has ${wallet.warnings} warnings.\n`;
+            } else {
+              console.warn(`Wallet entry with null userId found:`, wallet);
+            }
           }
         }
     
