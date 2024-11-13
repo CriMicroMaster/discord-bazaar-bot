06d5a9ee2b47e1eaed16991be9d1fa3ad663ca36
CriMicroMaster <lukasimhof06@hotmail.com>
2024-11-13 14:50:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits, ActivityType, EmbedBuilder, Colors, PermissionsBitField, ButtonBuilder, ButtonStyle, ActionRowBuilder, Time } = require("discord.js");
+const { Client, GatewayIntentBits, ActivityType, EmbedBuilder, Colors, PermissionsBitField, ButtonBuilder, ButtonStyle, ActionRowBuilder, Time, Events } = require("discord.js");
 const { Sequelize, DataTypes, Op } = require("sequelize");
 const slash_deploy = require("./slash_deploy.js")
 const keep_alive = require("./keep_alive.js");
@@ -1000,7 +1000,7 @@ client.on("ready", (c) => {
   setInterval(updateActivity, 15 * 60 * 1000); // 15 minutes
 });
 
-client.on('messageReactionAdd', async (reaction, user) => {
+client.on(Events.MessageReactionAdd, async (reaction, user) => {
     if (user.bot) return;
 
     console.log("Added");
